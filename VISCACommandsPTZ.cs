9db943b5d84bade1a7d1fafba588584a32551fda
zd4 <53253483+zd4@users.noreply.github.com>
2020-12-01 22:31:23 +0100

    Vereinfachungen für nur eine Kamera; Nicht unterstützte Funktionen entfernt

--- VISCACommandsPTZ.cs
+++ VISCACommandsPTZ.cs
@@ -1,65 +1,68 @@
+﻿//aus https://github.com/jitenshap/SONY_VISCA_dotnet/tree/master/VISCALib
+//angepasst für Cam1
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace VISCALib
+namespace myVISCACommands
 {
+    
+
     public class VISCACommands
     {
         byte[] setAddress = { 0x88, 0x30, 0x01, 0xff };
         byte[] ifClear = { 0x88, 0x01, 0x00, 0x01, 0xff };
+        static byte CamId=(byte) (0x81);
+        
 
-        private static byte[] setCamId(byte[] buf, int camId)
-        {
-            buf[0] = (byte) (0x80 + camId);
-            return buf;
-        }
-
-        public static byte[] cmdCancel(int camId, int socketId)
+/* 
+        public static byte[] cmdCancel( int socketId)
         {
             byte[] buf = { 0x80, 0x20, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             buf[1] = (byte) (socketId + 0x20);
             buf[2] = 0xff;
             return buf;
         }
-        public static byte[] swOn(int camId)
+ */        public static byte[] swOn(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x00, 0x02, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
-        public static byte[] swOff(int camId)
+        public static byte[] swOff(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x00, 0x03, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
-        public static byte[] zoomStop(int camId)
+        public static byte[] zoomStop(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x07, 0x00, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
-        public static byte[] zoomTele(int camId, int speed)
+        public static byte[] zoomTele( int speed)
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x07, 0x20, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             buf[4] += (byte)speed;
             return buf;
         }
-        public static byte[] zoomWide(int camId, int speed)
+        public static byte[] zoomWide(  int  speed)
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x07, 0x30, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             buf[4] += (byte)speed;
             return buf;
         }
-        public static byte[] zoomPos(int camId, int pos)
+        public static byte[] zoomPos(  int  pos)
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x47, 0x00, 0x00, 0x00, 0x00, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             if(pos >> 0x0c > 0x0)
             {
                 buf[4] = (byte)(pos >> 0x0c);
@@ -78,30 +81,30 @@ namespace VISCALib
             buf[7] = (byte)pos;
             return buf;
         }
-        public static byte[] focusStop(int camId)
+        public static byte[] focusStop(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x08, 0x00, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
-        public static byte[] focusFar(int camId, int speed)
+        public static byte[] focusFar(  int  speed)
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x08, 0x20, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             buf[4] += (byte)speed;
             return buf;
         }
-        public static byte[] focusNear(int camId, int speed)
+        public static byte[] focusNear(  int  speed)
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x08, 0x30, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             buf[4] += (byte)speed;
             return buf;
         }
-        public static byte[] focusPos(int camId, int pos)
+        public static byte[] focusPos(  int  pos)
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x47, 0x00, 0x00, 0x00, 0x00, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             if (pos >> 0x0c > 0x0)
             {
                 buf[4] = (byte)(pos >> 0x0c);
@@ -120,75 +123,75 @@ namespace VISCALib
             buf[7] = (byte)pos;
             return buf;
         }
-        public static byte[] focusInf(int camId)
+       /*  public static byte[] focusInf(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x18, 0x02, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
-        }
-        public static byte[] focusAF(int camId)
+        } */
+        public static byte[] focusAF(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x38, 0x02, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
-        public static byte[] focusMF(int camId)
+        public static byte[] focusMF(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x38, 0x03, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
-        public static byte[] focusToggle(int camId)
+        public static byte[] focusToggle(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x38, 0x10, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
-        public static byte[] focusOnePush(int camId)
+        /* public static byte[] focusOnePush(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x18, 0x01, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
-        }
-        public static byte[] whiteAuto(int camId)
+        } */
+        public static byte[] whiteAuto(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x00, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
 
-        public static byte[] whiteIndoor(int camId)
+        public static byte[] whiteIndoor(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x01, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
 
-        public static byte[] whiteOutdoor(int camId)
+        public static byte[] whiteOutdoor(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x02, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
 
-        public static byte[] whiteOnePush(int camId)
+        public static byte[] whiteOnePush(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x03, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
 
-        public static byte[] whiteManual(int camId)
+        public static byte[] whiteManual(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x05, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
 
-        public static byte[] whiteTrigOnePush(int camId)
+        public static byte[] whiteTrigOnePush(  )
         {
             byte[] buf = { 0x80, 0x01, 0x04, 0x10, 0x05, 0xff };
-            buf = setCamId(buf, camId);
+            buf[0] = CamId;
             return buf;
         }
     }
