fe2a8dba108411f359ec0ecb0dd6b5dacc6aec0d
Matthias Beck <beck.matthias@googlemail.com>
2020-12-07 21:45:17 +0100

    add GUI-Asset
    Joystick etc.

---
+++ Assets/Scripts/PTZ.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using myVISCACommands;
+
+public class PTZ : MonoBehaviour
+{
+    //private
+     UdpConnection connection; 
+    public string sendIp = "192.168.178.88";
+        public int sendPort = 1259;
+        int receivePort = 11000;
+        float sendtime;
+        public Text antwort;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     connection = new UdpConnection();   
+     connection.StartConnection(sendIp, sendPort, receivePort);
+     sendtime=0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var message in connection.getMessages())
+        {if (sendtime==0f) {print ("Antwort ohne (neue) Anfrage: "+ message);
+        antwort.text= ("Antwort ohne (neue) Anfrage: "+ message);}
+        else{print ("Antwort nach "+ (Time.time - sendtime).ToString() + ": "+ message);
+        antwort.text=("Antwort nach "+ (Time.time - sendtime).ToString() + ": "+ message);
+        sendtime=0f;
+        }
+        //connection.Send("Hi!");
+    }}
+            void OnDestroy()
+    {
+        connection.Stop();
+    
+    }
+
+    public void SwitchPosition(int i){
+        print("Schwenk zu Position "+i);
+        antwort.text=("Schwenk zu Position "+i);
+        switch( i){
+        case 1:
+        connection.SendString("8101043F0201FF");
+        break;
+        case 2:
+        connection.SendString("8101043F0202FF");
+        break;
+        default:
+        print("Fehler");
+        break;}
+        sendtime=Time.time;
+       
+    }
+    public void FocusPos(int i){
+        print("Visca Focus Pos "+i);
+        connection.Send(VISCACommands.focusPos(i));
+    }
+
+    public void ZoomTele(){
+        print("Visca Tele");
+        connection.Send(VISCACommands.zoomTele(2));
+    }
+}
---
+++ Assets/Scripts/VISCACommandsPTZ.cs
@@ -0,0 +1,198 @@
+﻿//aus https://github.com/jitenshap/SONY_VISCA_dotnet/tree/master/VISCALib
+//angepasst für Cam1
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace myVISCACommands
+{
+    
+
+    public class VISCACommands
+    {
+        byte[] setAddress = { 0x88, 0x30, 0x01, 0xff };
+        byte[] ifClear = { 0x88, 0x01, 0x00, 0x01, 0xff };
+        static byte CamId=(byte) (0x81);
+        
+
+/* 
+        public static byte[] cmdCancel( int socketId)
+        {
+            byte[] buf = { 0x80, 0x20, 0xff };
+            buf[0] = CamId;
+            buf[1] = (byte) (socketId + 0x20);
+            buf[2] = 0xff;
+            return buf;
+        }
+ */        public static byte[] swOn(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x00, 0x02, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+        public static byte[] swOff(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x00, 0x03, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+        public static byte[] zoomStop(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x07, 0x00, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+        public static byte[] zoomTele( int speed)
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x07, 0x20, 0xff };
+            buf[0] = CamId;
+            buf[4] += (byte)speed;
+            return buf;
+        }
+        public static byte[] zoomWide(  int  speed)
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x07, 0x30, 0xff };
+            buf[0] = CamId;
+            buf[4] += (byte)speed;
+            return buf;
+        }
+        public static byte[] zoomPos(  int  pos)
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x47, 0x00, 0x00, 0x00, 0x00, 0xff };
+            buf[0] = CamId;
+            if(pos >> 0x0c > 0x0)
+            {
+                buf[4] = (byte)(pos >> 0x0c);
+                pos -= pos >> 0x0c;
+            }
+            if (pos >> 0x08 > 0x0)
+            {
+                buf[5] = (byte)(pos >> 0x08);
+                pos -= pos >> 0x08;
+            }
+            if (pos >> 0x04 > 0x0)
+            {
+                buf[6] = (byte)(pos >> 0x04);
+                pos -= pos >> 0x04;
+            }
+            buf[7] = (byte)pos;
+            return buf;
+        }
+        public static byte[] focusStop(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x08, 0x00, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+        public static byte[] focusFar(  int  speed)
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x08, 0x20, 0xff };
+            buf[0] = CamId;
+            buf[4] += (byte)speed;
+            return buf;
+        }
+        public static byte[] focusNear(  int  speed)
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x08, 0x30, 0xff };
+            buf[0] = CamId;
+            buf[4] += (byte)speed;
+            return buf;
+        }
+        public static byte[] focusPos(  int  pos)
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x47, 0x00, 0x00, 0x00, 0x00, 0xff };
+            buf[0] = CamId;
+            if (pos >> 0x0c > 0x0)
+            {
+                buf[4] = (byte)(pos >> 0x0c);
+                pos -= pos >> 0x0c;
+            }
+            if (pos >> 0x08 > 0x0)
+            {
+                buf[5] = (byte)(pos >> 0x08);
+                pos -= pos >> 0x08;
+            }
+            if (pos >> 0x04 > 0x0)
+            {
+                buf[6] = (byte)(pos >> 0x04);
+                pos -= pos >> 0x04;
+            }
+            buf[7] = (byte)pos;
+            return buf;
+        }
+       /*  public static byte[] focusInf(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x18, 0x02, 0xff };
+            buf[0] = CamId;
+            return buf;
+        } */
+        public static byte[] focusAF(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x38, 0x02, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+        public static byte[] focusMF(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x38, 0x03, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+        public static byte[] focusToggle(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x38, 0x10, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+        /* public static byte[] focusOnePush(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x18, 0x01, 0xff };
+            buf[0] = CamId;
+            return buf;
+        } */
+        public static byte[] whiteAuto(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x00, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+
+        public static byte[] whiteIndoor(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x01, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+
+        public static byte[] whiteOutdoor(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x02, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+
+        public static byte[] whiteOnePush(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x03, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+
+        public static byte[] whiteManual(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x35, 0x05, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+
+        public static byte[] whiteTrigOnePush(  )
+        {
+            byte[] buf = { 0x80, 0x01, 0x04, 0x10, 0x05, 0xff };
+            buf[0] = CamId;
+            return buf;
+        }
+    }
+}
